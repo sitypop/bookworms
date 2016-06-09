68d9b2c1262921d7e16ef31e0b449913330bf155
sitypop <sityemilyshah@gmail.com>
2016-06-09 22:13:47 +0100

    Creates  POST request to rails API to add a book club, missing bower components my end

--- app/controllers/book_clubs_controller.rb
+++ app/controllers/book_clubs_controller.rb
@@ -1,5 +1,21 @@
 class BookClubsController < ApplicationController
+  skip_before_filter  :verify_authenticity_token
+
   def index
     respond_with BookClub.all
   end
+
+  def create
+    @bookclub = BookClub.create!(bookclub_params)
+
+    respond_to do |format|
+      format.json { render json: @bookclub, status: :created, location: @bookclub }
+    end
+  end
+
+  def bookclub_params
+    params.require(:params["params"]).permit(:name, :location, :description, :genre, :day, :time, :book)
+  end
+
+
 end
