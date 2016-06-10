8394866ebd10e424574bc62ece01303e093b53a7
Matthew Ward <iammatthewward@gmail.com>
2016-06-10 12:44:52 +0100

    Refactors styling

--- app/assets/javascripts/services/BookClubService.js
+++ app/assets/javascripts/services/BookClubService.js
@@ -19,7 +19,7 @@ bookWormApp.service("BookClubService", [ "$http", "BookClubFactory", function($h
   }
 
   self.addBookClub = function(bookclub){
-    $http.post("/book_clubs", {params: { name: bookclub.name, genre: bookclub.genre, location: bookclub.location, description: bookclub.description, day: bookclub.day, time: bookclub.time, book: bookclub.book } });
+    return $http.post("/book_clubs", {params: { name: bookclub.name, genre: bookclub.genre, location: bookclub.location, description: bookclub.description, day: bookclub.day, time: bookclub.time, book: bookclub.book } });
   };
 
 
