5f7f7036ba928fd3e84006454da50342137a3ee7
sitypop <sityemilyshah@gmail.com>
2016-07-05 16:00:10 +0100

    Removes e from navCtrl.js

--- app/assets/javascripts/controllers/navCtrl.js
+++ app/assets/javascripts/controllers/navCtrl.js
@@ -6,15 +6,15 @@ bookWormApp.controller('NavCtrl', ['$scope', 'Auth', function($scope, Auth){
     $scope.user = user;
   });
 
-  $scope.$on('devise:new-registration', function (e, user){
+  $scope.$on('devise:new-registration', function (user){
     $scope.user = user;
   });
 
-  $scope.$on('devise:login', function (e, user){
+  $scope.$on('devise:login', function (user){
     $scope.user = user;
   });
 
-  $scope.$on('devise:logout', function (e, user){
+  $scope.$on('devise:logout', function (user){
     $scope.user = {};
   });
 }]);
